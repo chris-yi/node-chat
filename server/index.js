9a65c7ad4d7c1119f8e64faeb2b351dad3b2d179
chris-yi <kri5to.yi@gmail.com>
2017-10-04 15:32:23 -0600

    finished project

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require("express"),
+      bodyParser = require("body-parser")
+      messCtrl = require("./controllers/messages_controller.js")
+
+const app = express();
+const port = 3000;
+
+
+app.use(bodyParser.json());
+
+app.get("/api/messages", messCtrl.read);
+app.post("/api/messages", messCtrl.create)
+app.put("/api/messages/:id", messCtrl.update)
+app.delete("/api/messages/:id", messCtrl.delete)
+
+
+app.listen( port, () => {
+    console.log(`Server listening on port ${port}`)
+})
+
